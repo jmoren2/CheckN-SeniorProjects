44ac6e1f64a1a28ae34467a9fed21da39a602480
jmoren2 <jmoren2@github.com>
2018-04-08 21:40:12 -0700

    began createPost

--- js/posts-api/createPost.js
+++ js/posts-api/createPost.js
@@ -1,12 +1,25 @@
 'use strict';
 
 module.exports.createPost = (ddb, event, context, callback) => {
-    var response = {
-        statusCode: 500,
-        body: JSON.stringify({
-            statusCode: 500,
-            message: 'endpoint not implemented yet'
-        })
+
+    var post = {
+        Item: JSON.parse(event.body),
+        TableName: 'posts'
+
     }
-    return callback(null, response);
+
+    console.log(post);
+    
+    ddb.put(post, function(error, data) {
+        if(error)
+          callback(null, {
+            statusCode: 500,
+            body: JSON.stringify({message: 'Post creation failed. Error: ' + error})
+          });
+        else
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({message: 'Post Created!'})
+          });
+      });
 }
