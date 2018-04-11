5815ce9c9e7403f3698fe5e1b4d1ec4be67d6bc3
Jonathan Jensen <jojensen@pdx.edu>
2018-04-11 16:27:57 -0700

    successfully update post content and update timestamp

--- js/posts-api/updatePostContent.js
+++ js/posts-api/updatePostContent.js
@@ -1,36 +1,48 @@
 'use strict';
-  
-module.exports.updatePostContent = (ddb, event, context, callback) => {
+const moment = require('moment');
 
-    //TODO figure out how to get parameters
-    var tableName = "posts";
-    var newContent = "testContent";
-    var item = event.JSON(event.body);
-    var params = {
-        TableName: tableName,
-        Key:{
-            "id": event.pathParamaters.postId
+module.exports.updatePostContent = (ddb, event, context, callback) => {
+    if(event.body !== null && event.body !== undefined){
+        var tableName = "posts";
+        var item = JSON.parse(event.body);
+        var updatedTimeStamp = moment().toISOString();
+        console.log(item);
+        var postId = event.pathParameters.postId
+        console.log(postId)
+        var params = {
+            TableName: tableName,
+            Key:{
+                "postId": postId
+            },
+        UpdateExpression: "set content = :content, #time =:timestamp",
+        ExpressionAttributeValues:{
+            ":content":item.content,
+            ":timestamp":updatedTimeStamp
         },
-    UpdateExpression: "set content = :content",
-    ExpressionAttributeValues:{
-        ":content":newContent
-    },
-    ReturnValues:"UPDATED_NEW"
-};
+        ExpressionAttributeNames: {
+            "#time": "timestamp"
+        },     
+        ReturnValues:"UPDATED_NEW"
+        };
 
     console.log("Updating the content of a Post...");
     ddb.update(params, function(error, data) {
-       if(error){
+      if(error)
         callback(null, {
           statusCode: 500,
-          body: JSON.stringify({message: 'Updating Post content Error: ' + error})
+          body: JSON.stringify({message: 'Update Post Content failed. Error: ' + error})
         });
-       }
-      else{
+      else
         callback(null, {
           statusCode: 201,
           body: JSON.stringify({message: 'Post Content Updated.'})
         });
-      }
     });
+    }
+    else{
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify({message: 'Post content updated failed. Error: ' + error})
+        });
+    }
 }
