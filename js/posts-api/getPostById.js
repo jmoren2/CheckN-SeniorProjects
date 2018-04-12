434370a3f364ebc29e76a58fc8263fb5f3a9e6ca
Mike <blauv@pdx.edu>
2018-04-11 18:56:42 -0700

    code cleanup

--- js/posts-api/getPostById.js
+++ js/posts-api/getPostById.js
@@ -4,18 +4,17 @@ module.exports.getPostById = (ddb, event, context, callback) => {
     var response = 0;
     console.log('parameter content: ' + event.pathParameters.postId);
     ddb.get({TableName: 'posts', Key: {'postId': event.pathParameters.postId}}, function(error, data) {
-        if(error) {
+        if(error)
             response = {
                 statusCode: 500,
                 message: JSON.stringify({message: 'getPostById failed. Error: ' + error})
             };
-        } else {
+        else
             console.log('response: ' + JSON.stringify(data));
             response = {
                 statusCode: 200,
                 body: JSON.stringify({post: data.Item})
             };
-        }
         return callback(null, response);
     });
 }
