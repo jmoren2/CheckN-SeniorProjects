7f42776ff07c0b10a08caf4a087bfe01bf3c8305
Jonathan Jensen <jojensen@pdx.edu>
2018-04-14 15:57:02 -0700

    added update comment function

--- js/comments-api/updateComment.js
+++ js/comments-api/updateComment.js
@@ -1,12 +1,53 @@
 'use strict';
+const moment = require('moment');
 
 module.exports.updateComment = (ddb, event, context, callback) => {
-    var response = {
-        statusCode: 500,
-        body: JSON.stringify({
-            statusCode: 500,
-            message: 'endpoint not implemented yet'
-        })
+    if(event.body !== null && event.body !== undefined){
+        var tableName = "comments";
+        var item = JSON.parse(event.body);
+        var updatedTimeStamp = moment().toISOString();
+        console.log(item);
+        var postId = event.pathParameters.postId
+        var commentId = event.pathParameters.commentId
+        console.log(postId)
+        var params = {
+            TableName: tableName,
+            Key:{
+                "commentId": commentId
+            },
+        UpdateExpression: "set content = :content, #time =:timestamp, userId = :userId, positiveVoters = :positiveVoters, neturalVoters = :neutralVoters , negativeVoters = :negativeVoters",
+        ExpressionAttributeValues:{
+            ":content":item.content,
+            ":timestamp":updatedTimeStamp,
+            ":userId" : item.userId,
+            ":positiveVoters" : item.positiveVoters,
+            ":neutralVoters" : item.neutralVoters,
+            ":negativeVoters" : item.negativeVoters
+        },
+        ExpressionAttributeNames: {
+            "#time": "timestamp"
+        },     
+        ReturnValues:"UPDATED_NEW"
+        };
+
+    console.log("Updating the content of a Post...");
+    ddb.update(params, function(error, data) {
+      if(error)
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify({message: 'Update Post Content failed. Error: ' + error})
+        });
+      else
+        callback(null, {
+          statusCode: 201,
+          body: JSON.stringify({message: 'Post Content Updated.'})
+        });
+    });
     }
-    return callback(null, response);
+    else{
+        callback(null, {
+            statusCode: 500,
+            body: JSON.stringify({message: 'Post content updated failed. Error: ' + error})
+        });
+    }
 }
