0c3f57083ba659057cc75bf00c3357605f5cef25
Andrew <blackdahlia1147@gmail.com>
2018-04-15 23:11:26 -0700

    Cleaned up multiple formatting issues and took out unnecessary junk.  Still not functioning just yet

--- frontend/src/PostEditor.js
+++ frontend/src/PostEditor.js
@@ -5,31 +5,25 @@ class PostEditor extends Component{//Initial State
         super();
         this.state = {postContent: ""};
     }
-}
 
-componentDidMount(){
-    fetch('https://67kbssnx29.execute-api.us-west-2.amazonaws.com/dev/posts/{postId}')//NEED postId to be fetched
-    .then(postInfo => {return postInfo.json();})//Grab the JSON given by the API
-    .then(data => {
-        let postBody = data.postInfo.map((body) => {//Map over data
-            //WORK IN PROGRESS THIS WAS A GUESS
-            return(
-                <div key={body.postInfo}>
-                    <p>{body.postInfo}</p>
-                </div>
-            )
-        })
-        this.setState({postContent: postContent});
-        console.log("STATE:  ", this.state.postContent)
-    })
-}
+    componentDidMount(){
+        fetch('https://67kbssnx29.execute-api.us-west-2.amazonaws.com/dev/posts/{postId}')//Queries the API for a post weith  specified ID
+        .then(results => {return results.json();});//Saves the response as JSON
+        return(//places the body of the post in an input field for editing
+            <div key={results.post}>
+                <input type="text" value="{results.post}">
+                </input>
+            </div>
+        )
+        this.setState({postContent: postContent});//update the state (unsure of this spot)
+        console.log("STATE:  ", this.state.postContent);
+    }
 
     render(){
         return(
             <div>
-                <div>
-                    {this.state.postContent}
-                </div>
+                {this.state.postContent}
             </div>
         )
     }
+}
