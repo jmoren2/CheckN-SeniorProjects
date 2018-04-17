727a54924db991bad264889470d6f5da9229d874
Jonathan Jensen <jojensen@pdx.edu>
2018-04-16 17:07:35 -0700

    change error message

--- js/posts-api/updatePost.js
+++ js/posts-api/updatePost.js
@@ -51,7 +51,7 @@ module.exports.updatePost = (ddb, event, context, callback) => {
     else{
         callback(null, {
             statusCode: 500,
-            body: JSON.stringify({message: 'Post content updated failed. Error: ' + error})
+            body: JSON.stringify({message: 'Post content updated failed. Error: JSON body is empty or undefined' })
         });
     }
 }
