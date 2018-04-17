ddfe1db9f169b296b0e586456f01233fdac4d252
MattRipley <rip3@pdx.edu>
2018-04-16 17:14:06 -0700

    what we worked on today

--- frontend/src/PostEditor.js
+++ frontend/src/PostEditor.js
@@ -7,16 +7,28 @@ class PostEditor extends Component{//Initial State
     }
 
     componentDidMount(){
-        fetch('https://67kbssnx29.execute-api.us-west-2.amazonaws.com/dev/posts/{postId}')//Queries the API for a post weith  specified ID
-        .then(results => {return results.json();});//Saves the response as JSON
-        return(//places the body of the post in an input field for editing
-            <div key={results.post}>
+        var results;
+        fetch('https://67kbssnx29.execute-api.us-west-2.amazonaws.com/dev/posts/dabda155-3d89-4cf8-b705-3301fe361249',{
+            headers: {
+                'content-type': 'application/json'
+            },
+            method: 'GET',
+            mode: 'cors',
+        })//Queries the API for a post weith  specified ID
+        .then(results => {
+            console.log('r: ' + results);
+            return results.json();})//Saves the response as JSON
+        .then(data => {
+            console.log('d: ' + data);
+            this.setState({postContent: '1'});//update the state (unsure of this spot)
+            console.log("STATE:  ", this.state.postContent);
+            return(//places the body of the post in an input field for editing
+            <div key={data}>
                 <input type="text" value="{results.post}">
                 </input>
             </div>
-        )
-        this.setState({postContent: postContent});//update the state (unsure of this spot)
-        console.log("STATE:  ", this.state.postContent);
+        )})
+
     }
 
     render(){
@@ -24,6 +36,7 @@ class PostEditor extends Component{//Initial State
             <div>
                 {this.state.postContent}
             </div>
-        )
+        );
     }
 }
+ReactDOM.render(<PostEditor />, document.getElementById('root'));
