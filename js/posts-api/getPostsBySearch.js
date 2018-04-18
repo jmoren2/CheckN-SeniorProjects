70bcae9afd276af0df53e77cf1f989b6afad2c43
Mike <blauv@pdx.edu>
2018-04-17 19:13:52 -0700

    initial test implementation

--- js/posts-api/getPostsBySearch.js
+++ js/posts-api/getPostsBySearch.js
@@ -1,12 +1,46 @@
 'use strict';
+const fail = require('./responses').postsFail;
+const success = require('./responses').multiPostSuccess;
 
 module.exports.getPostsBySearch = (ddb, event, context, callback) => {
-    var response = {
-        statusCode: 500,
-        body: JSON.stringify({
-            statusCode: 500,
-            message: 'endpoint not implemented yet'
+
+    console.log(event.body);
+
+    // initial test case
+    var params = {
+        TableName: 'posts',
+        KeyConditionExpression: "#state = :state",
+        ExpressionAttributeNames: {
+            "#state": "state"
+        },
+        ExpressionAttributeValues: {
+            ":state": "OPEN"
+        }
+    };
+
+    console.log(params);
+
+    var items = [];
+
+    // query loop in case of multiple pages
+    var queryExecute = function(callback) {
+        ddb.query(params, function(err, data) {
+            if(err)
+                return fail(500, 'getPostsBySearch failed. Error: ' + err, callback);
+            else {
+                console.log(data);
+
+                items = items.concat(data.Items);
+
+                if(data.LastEvaluatedKey) {
+                    params.ExclusiveStartKey = result.LastEvaluatedKey;
+                    queryExecute(callback);
+                }
+                else
+                    return success(200, items, callback);
+            }
         })
-    }
-    return callback(null, response);
-}
+    };
+
+    queryExecute(callback);
+};
