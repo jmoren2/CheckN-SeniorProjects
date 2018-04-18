dc2d05523fef3c6f29ebc072e709cda6d112fe43
Kristen Bateson <k.bateson@comcast.net>
2018-04-17 20:01:09 -0700

    create post now works yay

--- frontend/src/CreatePost.js
+++ frontend/src/CreatePost.js
@@ -6,32 +6,51 @@ class CreatePost extends Component{
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.returnedID = null;
+        this.state = {title: '', content: '', returnedId: ''};
+        this.handleChangeTitle = this.handleChangeTitle.bind(this);
+        this.handleChangeContent = this.handleChangeContent.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(event){
         event.preventDefault();
-        const data = new FormData(event.target);
+        console.log('state.title: ' + this.state.title);
+        console.log('state.content: ' + this.state.content);
+        const data = {title: this.state.title, content: this.state.content};
+        console.log('data: ' + JSON.stringify(data));
 
         fetch('https://95sbuermt6.execute-api.us-west-2.amazonaws.com/dev/posts', {
             method: 'POST',
             body: JSON.stringify(data),
         })
         .then(result => {
-            console.log(result);
+            console.log('result: ' + JSON.stringify(result));
             return result.json()
         })
-        .then(data => {
-            console.log(data);
+        .then(response => {
+            console.log('response: ' + JSON.stringify(response));
+            this.setState({returnedId: response.post.postId});
+            console.log(this.state.returnedId);
+            console.log(response.post);
+            console.log(response.post.postId);
         });
+    }
+
+    handleChangeTitle(event) {
+        this.setState({title: event.target.value});
+    }
+
+    handleChangeContent(event) {
+        this.setState({content: event.target.value});
     }
 
     render(){
         return(
             <form onSubmit={this.handleSubmit}>
-                title<input id="title" name="title" type="text"/>
-                content<input id="content" name="content" type="text"/>
+                title<input type="title" value={this.state.value} onChange={this.handleChangeTitle} type="text" />
+                content<input type="content" value={this.state.value} onChange={this.handleChangeContent} type="text" />
 
-                {/* <Link to={`post/${this.returnedID}`}> */}
+                {/* <Link to={`post/${this.state.returnedID}`}> */}
                     <button>Send data!</button>
                 {/* </Link> */}
             </form>
