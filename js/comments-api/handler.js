05a7c26545d9ebc261445ef1d4915e9eb7dff415
Kristen Bateson <k.bateson@comcast.net>
2018-04-17 18:40:54 -0700

    moving require statements inside functions

--- js/comments-api/handler.js
+++ js/comments-api/handler.js
@@ -1,28 +1,28 @@
 'use strict';
 const AWS = require('aws-sdk');
-const getCommentsByPostId = require('./getCommentsByPostId').getCommentsByPostId
-const getCommentsBySearch = require('./getCommentsBySearch').getCommentsBySearch
-const createComment = require('./createComment').createComment;
-const deleteComment = require('./deleteComment').deleteComment;
-const updateComment = require('./updateComment').updateComment;
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.getCommentsByPostId = (event, context, callback) => {
+  const getCommentsByPostId = require('./getCommentsByPostId').getCommentsByPostId
   getCommentsByPostId(ddb, event, context, callback);
 };
 
 module.exports.getCommentsBySearch = (event, context, callback) => {
+  const getCommentsBySearch = require('./getCommentsBySearch').getCommentsBySearch
   getCommentsBySearch(ddb, event, context, callback);
 };
 
 module.exports.createComment = (event, context, callback) => {
+  const createComment = require('./createComment').createComment;
   createComment(ddb, event, context, callback);
 }
 
 module.exports.updateComment = (event, context, callback) => {
+  const updateComment = require('./updateComment').updateComment;
   updateComment(ddb, event, context, callback);
 }
 
 module.exports.deleteComment = (event, context, callback) => {
+  const deleteComment = require('./deleteComment').deleteComment;
   deleteComment(ddb, event, context, callback);
 }
