4628bfb822c60d518ff83a6b808d783c54922a6c
David Han <jinghan@pdx.edu>
2018-04-17 17:26:51 -0700

    Fixed deletePost

--- js/posts-api/deletePost.js
+++ js/posts-api/deletePost.js
@@ -1,11 +1,6 @@
 'use strict';
-<<<<<<< Updated upstream
-const success = require('./responses.js').deletePostSuccess;
-const fail = require('./responses.js').postsFail;
-=======
 const deletePostSuccess = require('./responses').deletePostSuccess;
 const deletePostFail = require('./responses.js').postsFail;
->>>>>>> Stashed changes
 
 module.exports.deletePost = (ddb, event, context, callback) => {
     if (event.pathParameters !== null && event.pathParameters !== undefined) {
@@ -23,14 +18,6 @@ module.exports.deletePost = (ddb, event, context, callback) => {
 
             console.log("Attempting a conditional delete...");
     
-<<<<<<< Updated upstream
-    ddb.delete(params, function(err, data) {
-        if(err)
-            return fail(500, 'Delete Post failed. Error: ' + err, callback);
-        else
-            return success(callback);
-    });
-=======
             ddb.delete(params, function(err, data) {
                 if(err)
                     return deletePostFail(500, 'Delete Post failed. Error: ' + err, callback);
@@ -43,5 +30,4 @@ module.exports.deletePost = (ddb, event, context, callback) => {
     }
     else
         return deletePostFail(400, 'Delete Post failed.', callback);
->>>>>>> Stashed changes
 }
