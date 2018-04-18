e5692ce88f9ed501aad84afcc4816b5cb2110049
Mike <blauv@pdx.edu>
2018-04-18 02:49:00 -0700

    barebones working search. Searches content, supports 'AND' with '+'

--- js/posts-api/getPostsBySearch.js
+++ js/posts-api/getPostsBySearch.js
@@ -1,46 +1,94 @@
 'use strict';
-const fail = require('./responses').postsFail;
 const success = require('./responses').multiPostSuccess;
+const fail = require('./responses').postsFail;
 
 module.exports.getPostsBySearch = (ddb, event, context, callback) => {
 
-    console.log(event.body);
+    // pull search key(s) and user from the query string
+    if(event.queryStringParameters) {
+        var search = event.queryStringParameters.search;
+        var user = event.queryStringParameters.user;
+        console.log("Search string: " + search);
+        console.log("User string: " + user);
+    }
 
-    // initial test case
-    var params = {
-        TableName: 'posts',
-        KeyConditionExpression: "#state = :state",
-        ExpressionAttributeNames: {
-            "#state": "state"
-        },
-        ExpressionAttributeValues: {
-            ":state": "OPEN"
-        }
-    };
+    // todo: users not implemented yet
+    user = null;
 
-    console.log(params);
+    if(search || user) {
+        // todo: look up user first if 'user' field is defined, early exit if not resolved
 
-    var items = [];
+        // use + as AND operator when passing strings, of the form:
+        // /posts?search=key1+key2+...
+        var keyArr = search.split("+");
+        console.log(keyArr);
 
-    // query loop in case of multiple pages
-    var queryExecute = function(callback) {
-        ddb.query(params, function(err, data) {
-            if(err)
-                return fail(500, 'getPostsBySearch failed. Error: ' + err, callback);
-            else {
-                console.log(data);
+        var numKeys = 0;
+        var attVals = {};
+        var filter = "";
 
-                items = items.concat(data.Items);
+        // NOTE: dynamoDB is case sensitive. Possible solutions: redundant all lowercase searchable data
+        // Or, incorporate the elasticsearch framework.
 
-                if(data.LastEvaluatedKey) {
-                    params.ExclusiveStartKey = result.LastEvaluatedKey;
-                    queryExecute(callback);
-                }
-                else
-                    return success(200, items, callback);
+        // todo: currently only searches content, should optionally search title as well
+
+        // dynamically build a filter expression along with attribute values from provided search keys
+        for(var i=0; i < keyArr.length; ++i){
+            if(keyArr[i]){
+                var attKey = ":word" + i;
+                attVals[attKey] = keyArr[i];
+                if(numKeys > 0)
+                    filter += " AND ";
+                filter += "contains (#content, " + attKey + ")";
+                numKeys++;
             }
-        })
-    };
+        }
 
-    queryExecute(callback);
+        console.log("number of search keys:" + numKeys);
+
+        if(numKeys > 0) {
+            console.log(JSON.stringify(attVals));
+            console.log(filter);
+
+            var params = {
+                TableName: 'posts',
+                FilterExpression: filter,
+                ExpressionAttributeNames: {
+                    "#content": "content"
+                },
+                ExpressionAttributeValues: attVals
+            };
+
+            console.log(params);
+
+            var items = [];
+
+            // scan loop in case of multiple pages of results
+            var scanExecute = function (callback) {
+                ddb.scan(params, function (err, data) {
+                    if (err)
+                        return fail(500, 'getPostsBySearch failed. Error: ' + err, callback);
+                    else {
+                        console.log(data);
+
+                        items = items.concat(data.Items);
+
+                        if (data.LastEvaluatedKey) {
+                            params.ExclusiveStartKey = result.LastEvaluatedKey;
+                            scanExecute(callback);
+                        }
+                        else
+                            return success(200, items, callback);
+                    }
+                })
+            };
+            scanExecute(callback);
+        }
+        // if no search keys were found after string parse, abort scan with error
+        else
+            return fail(500, 'getPostsBySearch failed. Error: Bad search key(s) provided', callback);
+    }
+    // missing parameters
+    else
+        return fail(500, 'getPostsBySearch failed. Error: No user or search parameters specified', callback);
 };
