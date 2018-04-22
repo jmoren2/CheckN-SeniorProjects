3b5dd3df06309f1f4fe1f2611032a097c05858ec
MattRipley <rip3@pdx.edu>
2018-04-21 23:07:03 -0700

    FeedPage now loads a bunch of posts

--- frontend/src/FeedPage.js
+++ frontend/src/FeedPage.js
@@ -5,8 +5,44 @@ class FeedPage extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+            feed: <div>Feed Placeholder</div>
         }
+    }
+
+    componentDidMount(){//Queries the API for a post with specified ID
+        this.retrieveFeed();
+    }
+
+    retrieveFeed(){
+        fetch(`https://95sbuermt6.execute-api.us-west-2.amazonaws.com/dev/posts?search=a`, {
+                headers: {
+                    'content-type': 'application/json'
+                },
+                method: 'GET',
+        })
+        .then(feedResults => {
+            return feedResults.json();
+        })
+        .then(feedData => {
+            console.log('comments: ' + JSON.stringify(feedData));
+            return(this.generateFeed(feedData.posts));
+        })
+        .then(Feed => {
+            this.setState({feed: Feed});
+        })
+    }
+
+    generateFeed(posts){
+        var feed = posts.map((post) => {
+            return(
+                <Link to={`/post/${post.postId}`}>
+                    <div key={post.postId}>
+                        {post.content}
+                    </div>
+                </Link>
+            )
+        })
+        return feed;
     }
 
     //Temporary link to self
@@ -19,6 +55,7 @@ class FeedPage extends React.Component{
                 <Link to="/post/de70345f-d7ef-4baa-b97f-c5c0391d6dd1">
                     <button>View Post</button>
                 </Link>
+                {this.state.feed}
             </div>
         );
     }
