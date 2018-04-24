a59c889d38cdc562129d94117b200b38087f461c
Jonathan Jensen <jojensen@pdx.edu>
2018-04-24 15:28:47 -0700

    create and update tag endpoints

---
+++ js/tags-api/updateTag.js
@@ -0,0 +1,32 @@
+'use strict';
+const moment = require('moment');
+const success = require('./responses.js').singleTagSuccess;
+const fail = require('./responses.js').tagsFail;
+module.exports.updateTag = (ddb, event, context, callback) => {
+    if(event.body !== null && event.body !== undefined){
+        var tableName = "tags";
+        var item = JSON.parse(event.body);
+        var tagId = event.pathParameters.tagId
+        var params = {
+            TableName: tableName,
+            Key:{
+                "tagId": tagId
+            },
+        UpdateExpression: "set tagName = :tagName",
+        ExpressionAttributeValues:{
+            ":tagName":item.tagName,
+        },   
+        ReturnValues:"UPDATED_NEW"
+        };
+
+    ddb.update(params, function(error, data) {
+      if(error)
+        fail(500, 'Update Tag Content failed. Error: ' + error, callback)
+      else
+        success(201, data, callback)
+    });
+    }
+    else{
+        fail(500,'Tag updated failed. Error: JSON body is empty or undefined', callback);
+    }
+}
