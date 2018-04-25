73c890113accde30e7e63e429079847a42781e88
Josh Moreno <jmoren2@pdx.edu>
2018-04-24 22:56:28 -0700

    updated feed

--- frontend/src/FeedPage.js
+++ frontend/src/FeedPage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './Navbar.js'
+import ThumbsUp from 'react-icons/lib/fa/thumbs-up';
+import ThumbsDown from 'react-icons/lib/fa/thumbs-down';
+import Neutral from 'react-icons/lib/fa/arrows-h';
+import Moment from 'react-moment';
 
 class FeedPage extends React.Component{
     constructor(props){
@@ -35,17 +39,124 @@ class FeedPage extends React.Component{
     }
 
     generateFeed(posts){
+        
         var feed = posts.map((post) => {
-            return(
-                <Link to={`/post/${post.postId}`}>
-                <br />
-                    <div key={post.postId} className="card bg-light">
-                    <div class="card-block">
-                        {post.content}
-                        </div>
+            var pVoters = post.positiveVoters;
+            var nVoters = post.neutralVoters;
+            var negVoters = post.negativeVoters;
+
+            if(pVoters)
+            {
+                var positiveCount = pVoters.length;
+            }
+            else
+            {
+                var positiveCount = 0;
+            }
+            if(nVoters)
+            {
+                var neutralCount = nVoters.length;
+            }
+            else
+            {
+                var neutralCount = 0;
+            }
+            if(negVoters)
+            {
+                var negCount = negVoters.length;
+            }
+            else
+            {
+                var negCount = 0;
+            }
+            
+               return(
+
+                    //individual feed item
+                    <div className="container">
+
+                    
+
+                      <div className="row">
+
+                            <span className="col-sm">
+                      <button class="btn btn-primary btn-sm" type="submit">
+                            <ThumbsUp /> {positiveCount}
+                      </button>
+                      <br />
+                      <button class="btn btn-default btn-sm" type="submit">
+                            <Neutral /> {neutralCount}
+                      </button>
+                      <br />
+                      <button class="btn btn-danger btn-sm" type="submit">
+                            <ThumbsDown /> {negCount}
+                      </button>
+                      </span>
+
+                        <div className="col-sm-11">
+
+                    
+                    
+                    <div className="card bg-light">
+                                <Link to={`/post/${post.postId}`}>
+                                
+                                <div key={post.postId} >
+
+                                <div class="card-block">
+                                <div>
+                                    
+                                        
+                                </div>
+
+                                    {post.content} <br />
+                                    
+                                    
+                                    
+                                                                    
+                                    
+                                </div>
+                                
+                                
+                                </div>
+                            </Link>
+                            <div className="row">
+
+                                <div className="col-sm-4">
+                                <Moment format="YYYY/MM/DD HH:mm">
+                                {post.timestamp}
+                                </Moment>
+                                        
+
+                                </div>
+
+                                        <div className="col-sm-8">
+
+                                                 
+                                        {post.visibilityLevel}
+
+                                        </div>
+                                        
+
+                            </div>
                     </div>
-                </Link>
+
+
+
+                        </div>
+
+                      </div>
+                      <br/>
+                    </div>
+    
+                    
+                
+            
+            
+            
             )
+            
+            
+            
         })
         return feed;
     }
@@ -65,9 +176,9 @@ class FeedPage extends React.Component{
                             {/* <Link to="/create">
                             <button className='btn btn-info' type='submit'>Create Post</button> <br />
                 </Link> */}
-                {/* <Link to="/post/de70345f-d7ef-4baa-b97f-c5c0391d6dd1">
+                <Link to="/post/de70345f-d7ef-4baa-b97f-c5c0391d6dd1">
                 <button className='btn btn-info' type='submit'>View Post</button>
-                </Link> */}
+                </Link>
                         
                         {this.state.feed}
                         
