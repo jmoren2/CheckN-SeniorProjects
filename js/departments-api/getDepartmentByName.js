b3e3d80514f901758f65716e19684a0d13bb64bf
David Han <jinghan@pdx.edu>
2018-05-01 17:06:10 -0700

    getDepartmentByname

---
+++ js/departments-api/getDepartmentByName.js
@@ -0,0 +1,34 @@
+'use strict';
+const getSingleDepartmentSuccess = require('./responses').singleDepartmentSuccess;
+const getDepartmentFail = require('./responses').DepartmentsFail;
+
+module.exports.getDepartmentByName = (ddb, event, context, callback) => {
+    if (event.pathParameters !== null && event.pathParameters !== undefined) {
+        if (event.pathParameters.department !== undefined && 
+            event.pathParameters.department !== null && 
+            event.pathParameters.department !== "") {
+            console.log("Received proxy: " + event.pathParameters.department);
+
+            var name = event.pathParameters.department;
+            var params = {
+                TableName: "departments",
+                Key: {
+                    "department": name 
+                }
+            };
+
+            console.log("Attempting a conditional delete...");
+    
+            ddb.get(params, function(err, data) {
+                if(err)
+                    return getDepartmentFail(500,'get Department by name failed. Error: ' + err, callback);
+                else
+                    return getSingleDepartmentSuccess(200, data.Item, callback);
+            });
+        }
+        else
+            return getDepartmentFail(400, 'get Department by name failed.', callback);
+    }
+    else
+        return getDepartmentFail(400,'get Department by name failed', callback);
+}
