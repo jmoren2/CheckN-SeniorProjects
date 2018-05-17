154cd9e440ce902ed62d62ffd959b43599995bd1
Mike <blauv@pdx.edu>
2018-05-17 14:59:17 -0700

    fix comment/post update, implement user api

--- js/users-api/createUser.js
+++ js/users-api/createUser.js
@@ -4,28 +4,30 @@ const uuid = require('uuid');
 const success = require('./responses.js').singleUserSuccess;
 const fail = require('./responses.js').usersFail;
 
-module.exports.createUser = (ddb, event, context, callback) => {
+module.exports.createUser = (esClient, event, context, callback) => {
   if (event.body !== null && event.body !== undefined) {
 
-    var body = JSON.parse(event.body);
-    body.userId = uuid.v4();
+    var user = JSON.parse(event.body);
+    user.userId = uuid.v4();
     
 
-    var user = {
-        Item: body,
-        TableName: 'users'
-
-    }
+    var params = {
+      index: 'users',
+      type: 'user',
+      id: user.userId,
+      body: user
+    };
     
-    ddb.put(user, function(error, data) {
+    esClient.create(params, function(error, data) {
       if(error) {
-        return fail(500, 'User creation failed. Error: ' + error, callback);
+          console.log('User creation failed. Error: ' + JSON.stringify(error));
+          return fail(400, error, callback);
       } else {
-        console.log('data: ' + data);
-        return success(200, body, callback);
+          console.log('data: ' + JSON.stringify(data));
+          return success(200, data, callback);
       }
     });
   } else {
     return fail(500, 'User creation failed.' , callback)
   } 
-}
+};
