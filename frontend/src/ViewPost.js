e419cf9969138a1e00e1a9e2cafe5f36a0808fd0
Andrew <blackdahlia1147@gmail.com>
2018-05-18 18:58:44 -0700

    Initial effort to add opening/closing a post to ViewPost and disallow commenting or editing when closed

--- frontend/src/ViewPost.js
+++ frontend/src/ViewPost.js
@@ -33,17 +33,24 @@ class ViewPost extends Component{//Initial State
             votePhrase: "Please vote and add a comment if you'd like.",
             voteChoice: 'none',
             showModal: false,
-            userThatCommented: ""
+            userThatCommented: "",
+            postState: ""
         };
         this.posterID=null;
         this.posterName=null;
+        this.returnedID = null;
+        this.postInfo = {};
+
         this.handleOpenModal = this.handleOpenModal.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
-        this.returnedID = null;
         this.handleChangeComment = this.handleChangeComment.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.storeUser = this.storeUser.bind(this);
         this.editButton = this.editButton.bind(this);
+        this.changePostState = this.changePostState.bind(this);
+        this.closePost = this.closePost.bind(this);
+        this.openPost = this.openPost.bind(this);
+
         this.opacities = {POSITIVE: '0.6', NEUTRAL: '0.6', NEGATIVE: '0.6'};
         this.borders = {POSITIVE: '0px solid black' , NEUTRAL: '0px solid black', NEGATIVE: '0px solid black'};
         console.log("The user object passed in is: " + props.userObj);
@@ -76,6 +83,8 @@ class ViewPost extends Component{//Initial State
             var pVoters = data.post.positiveVoters;
             var nVoters = data.post.neutralVoters;
             var negVoters = data.post.negativeVoters;
+
+            this.state.postState = data.post.state;
 
             if(pVoters)
             {
@@ -170,6 +179,17 @@ class ViewPost extends Component{//Initial State
             console.log("IN RETRIEVE POST");
             console.log(data);
             console.log(data.post);
+            this.postInfo = {
+                title: data.post.title,
+                content: data.post.content,
+                positiveVoters: data.post.positiveVoters,
+                neutralVoters: data.post.neutralVoters,
+                negativeVoters: data.post.negativeVoters,
+                pinnedId: data.post.pinnedId,
+                state: data.post.state,
+                tagArray: data.post.tags,
+                visibilityLevel: data.post.visibilityLevel
+            };
             this.storeUser(data);//Continues the work in the function above
         });
     }
@@ -308,7 +328,8 @@ class ViewPost extends Component{//Initial State
     //Returns the response box
     //put inside of a method to later have this return nothing if the user already voted
     responseBox(){
-        return(
+        if(this.state.postState === "OPEN"){
+            return(
                 <div>
                 <label>{this.state.votePhrase}</label>
                 <form onSubmit={this.handleSubmit}>
@@ -329,7 +350,8 @@ class ViewPost extends Component{//Initial State
                     </div>
                 </form>
                 </div>
-        );
+            );
+        }
     }
 
     //Handles a click on post vote event
@@ -366,7 +388,7 @@ class ViewPost extends Component{//Initial State
       }
 
     editButton() {
-        if(this.props.userObj.userId === this.posterID) {
+        if(this.props.userObj.userId === this.posterID && this.state.postState === "OPEN") {
             return(
             <Link to={`/edit/${this.state.postID}`}>
             <button className='btn btn-info'>Edit Post</button>
@@ -375,6 +397,43 @@ class ViewPost extends Component{//Initial State
         else {
             return
         }
+    }
+
+    changePostState() {
+        if(this.state.postState === "OPEN") {
+            return(
+                <button className='btn btn-info' onClick = {this.closePost}>Close Post</button>//Button to close the post
+            );
+        }
+        else {
+            return(
+                <button className='btn btn-info' onClick = {this.openPost}>Reopen Post</button>//Button to reopen the post
+            );
+        }
+    }
+
+    closePost() {
+        this.postInfo.state = "CLOSED";
+        fetch(`https://c9dszf0z20.execute-api.us-west-2.amazonaws.com/prod/posts/${this.state.postID}`, {
+            method: 'PUT',
+            body: JSON.stringify(this.postInfo)//Stringify the data being sent
+        })
+        .then(response => {
+            return response.json()//Turn the response into a JSON object
+        });
+        //API call to close the post
+    }
+
+    openPost() {
+        this.postInfo.state = "OPEN";
+        fetch(`https://c9dszf0z20.execute-api.us-west-2.amazonaws.com/prod/posts/${this.state.postID}`, {
+            method: 'PUT',
+            body: JSON.stringify(this.postInfo)//Stringify the data being sent
+        })
+        .then(response => {
+            return response.json()//Turn the response into a JSON object
+        });
+        //API call to open the post
     }
 
     render(){
@@ -392,6 +451,7 @@ class ViewPost extends Component{//Initial State
                                     <div>
                                         {this.state.postContent}
                                         {this.editButton()}
+                                        {this.changePostState()}
                                     </div>
 
                                     <div>
