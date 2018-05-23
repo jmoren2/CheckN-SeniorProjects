ff628ef4b842b07f939a991b38fd8a9446b44c0e
Mike <blauv@pdx.edu>
2018-05-23 15:20:39 -0700

    cleaned up code for comments updating history/votes, added permissions to comment map for future improvements

--- js/comments-api/createComment.js
+++ js/comments-api/createComment.js
@@ -10,73 +10,15 @@ module.exports.createComment = (esClient, event, context, callback) => {
         var comment = JSON.parse(event.body);
         comment.commentId = uuid.v4();
         comment.timestamp = moment().toISOString();
-        var vote;
 
         // validate vote field
         if (comment.vote) {
-            vote = comment.vote;
-            if (!(vote === 'positive' || vote === 'negative' || vote === 'neutral')) {
-                vote = '';
+            let vote = comment.vote;
+            if (!(vote === 'positive' || vote === 'negative' || vote === 'neutral'))
                 delete comment.vote;
-            }
         }
 
-        var params = {
-            index: 'comments',
-            type: 'comment',
-            id: comment.commentId,
-            body: comment
-        };
-
-        esClient.create(params, function(error, data) {
-            if(error) {
-                console.log('Comment creation failed. error: ' + JSON.stringify(error));
-                return fail(400, error, callback);
-            } else {
-                if(vote){
-                    // construct json with string as key so we can upsert if value not initialized
-                    var temp = {};
-                    temp[vote] = 1;
-                    var params = {};
-                    if(comment.parentId){
-                        params = {
-                            index: 'comments',
-                            type: 'comment',
-                            id: comment.parentId
-                        }
-                    }
-                    else {
-                        params = {
-                            index: 'posts',
-                            type: 'post',
-                            id: comment.postId
-                        }
-                    }
-                    params.body = {
-                        script: 'ctx._source.voteCounts.' + vote + ' += 1',
-                        upsert: {
-                            voteCounts: temp
-                        }
-                    };
-
-                    console.log('vote update params: ' + JSON.stringify(params));
-                    esClient.update(params, function(error, data2){
-                        if(error){
-                            console.log('Comment creation failed to update votecount. error: ' + JSON.stringify(error));
-                            return fail(400, error, callback);
-                        }
-                        else{
-                            console.log('data: ' + JSON.stringify(data2));
-                            return success(200, comment, callback);
-                        }
-                    })
-                }
-                else {
-                    console.log('data: ' + JSON.stringify(data));
-                    return success(200, comment, callback);
-                }
-            }
-        });
+        return create(esClient, comment, callback);
     }
     else {
         let error = 'Comment creation failed. error: body empty';
@@ -84,3 +26,73 @@ module.exports.createComment = (esClient, event, context, callback) => {
         return fail(400, error, callback);
     }
 };
+
+function updateVote(esClient, comment, callback){
+    let vote = comment.vote;
+    if(vote){
+        // construct object with string as key so we can upsert if value not initialized
+        var temp = {};
+        temp[vote] = 1;
+        var params = {};
+        if(comment.parentId){
+            params = {
+                index: 'comments',
+                type: 'comment',
+                id: comment.parentId
+            }
+        }
+        else {
+            params = {
+                index: 'posts',
+                type: 'post',
+                id: comment.postId
+            }
+        }
+        params.body = {
+            script: 'ctx._source.voteCounts.' + vote + ' += 1',
+            upsert: {
+                voteCounts: temp
+            }
+        };
+
+        console.log('vote update params: ' + JSON.stringify(params));
+        esClient.update(params, function(error, data){
+            if(error){
+                console.log('Comment creation failed to update votecount. error: ' + JSON.stringify(error));
+                return fail(400, error, callback);
+            }
+            else{
+                console.log('data: ' + JSON.stringify(data));
+                return success(200, comment, callback);
+            }
+        })
+    }
+    else {
+        console.log('data: ' + JSON.stringify(data));
+        return success(200, comment, callback);
+    }
+}
+
+function create(esClient, comment, callback){
+    var params = {
+        index: 'comments',
+        type: 'comment',
+        id: comment.commentId,
+        body: comment
+    };
+
+    esClient.create(params, function(error, data) {
+        if(error) {
+            console.log('Comment creation failed. error: ' + JSON.stringify(error));
+            return fail(400, error, callback);
+        } else {
+            if(comment.vote){
+                return updateVote(esClient, comment, callback);
+            }
+            else {
+                console.log('data: ' + JSON.stringify(data));
+                return success(200, comment, callback);
+            }
+        }
+    });
+}
