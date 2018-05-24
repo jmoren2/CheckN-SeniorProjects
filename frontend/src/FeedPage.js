991d5b045f7198d5ce90e12cdc95423b585094ae
Andrew <blackdahlia1147@gmail.com>
2018-05-24 00:39:21 -0700

    Tweaks to the URLs and an additional check to changing the post state to require either the poster or an admin to show the button

--- frontend/src/FeedPage.js
+++ frontend/src/FeedPage.js
@@ -33,7 +33,7 @@ class FeedPage extends React.Component{
     }
 
     retrieveFeed(){
-        fetch(`https://mvea1vrrvc.execute-api.us-west-2.amazonaws.com/prod/posts?${this.searchQuery}`, {
+        fetch(`https://wjnoc9sykb.execute-api.us-west-2.amazonaws.com/dev/posts?${this.searchQuery}`, {
                 headers: {
                     'content-type': 'application/json'
                 },
@@ -87,7 +87,7 @@ class FeedPage extends React.Component{
         //grab actual user id
 
        
-       fetch(`https://wjnoc9sykb.execute-api.us-west-2.amazonaws.com/prod/posts/${postToBeVotedOn}`, {
+       fetch(`https://wjnoc9sykb.execute-api.us-west-2.amazonaws.com/dev/posts/${postToBeVotedOn}`, {
         method: 'PUT',
         body: JSON.stringify(post)
     })
@@ -138,7 +138,7 @@ class FeedPage extends React.Component{
             }
 
 
-            fetch(`https://wjnoc9sykb.execute-api.us-west-2.amazonaws.com/prod/posts/${postToBeVotedOn}`, {
+            fetch(`https://wjnoc9sykb.execute-api.us-west-2.amazonaws.com/dev/posts/${postToBeVotedOn}`, {
                 method: 'PUT',
                 body: JSON.stringify(post)
             })
@@ -189,7 +189,7 @@ class FeedPage extends React.Component{
                 console.log(post);
             }
 
-            fetch(`https://wjnoc9sykb.execute-api.us-west-2.amazonaws.com/prod/posts/${postToBeVotedOn}`, {
+            fetch(`https://wjnoc9sykb.execute-api.us-west-2.amazonaws.com/dev/posts/${postToBeVotedOn}`, {
                 method: 'PUT',
                 body: JSON.stringify(post)
             })
