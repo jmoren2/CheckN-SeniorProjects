7d05ce71a3fa15bb69ee9cd1a3a2a2230be8957e
Kristen Bateson <k.bateson@comcast.net>
2018-05-29 22:49:40 -0700

    updated README, fixed the about page css

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -20,19 +20,31 @@ class About extends React.Component{
             <div>
             <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC|Lato|Lobster" rel="stylesheet" />
             <Navbar />
-               <div className="text-center">
-                   
-                    Written by A-Team Capstone Winter-Spring 2018!
+                <div className="container">
+                    <div className="row">
+                        <div className="card card-1">
+                            <div className="col-sm" />
+                            <div id="desc" className="col-sm-12"><div className="card-body">
+                                <div className="text-center" id="title">About</div><br /><br />
+                                <p>CheckN is a platform for sharing ideas, gathering feedback, and solving problems. 
+                                Harness the resources you already have, by allowing easy communication inside teams as well as across multiple teams.</p>
 
-                    <div className="text-center">
+                                <p>Create your own custom surveys then analyze the results right on this site, or download your results in an easy to use .csv format</p>
 
-Kristen Bateson, Mike Blauvelt, Andy Grill, Jinghui Han, 
-Jonathan Jensen, Josh Moreno, and Matt Ripley
-
+                                <p>Use the admin reporting tools to see where the most activity is being centralized, on a per user or a per department basis.</p>
+                                
+                                <p>Easily manage users, departments, and roles from the admin console.</p>
+                                <br /><br />
+                                <div className="text-center" id="details">
+                                    Written by Portland State University Capstone A-Team, Winter-Spring 2017-18:
+                                    <br />
+                                    Kristen Bateson, Mike Blauvelt, Andy Grill, Jinghui Han, 
+                                    Jonathan Jensen, Josh Moreno, and Matt Ripley
+                                </div> 
+                            </div></div>
+                        </div>
                     </div>
-                   
-                </div> 
-
+                </div>
             </div>
 
         
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import LogInPage from './LogInPage.js';
 import FeedPage from './FeedPage.js';
